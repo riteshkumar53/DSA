857340aa0c5224e4a524899480b7f1a86b3af140
riteshkr53 <riteshkr10809@gmail.com>
2026-06-03 15:16:24 +0530

    two sum brute force solution

---
+++ STL/vector/2_sum.cpp
@@ -0,0 +1,44 @@
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return the indices
+// of the two numbers such that they add up to target.
+// You may assume exactly one solution exists.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void two_sum(vector<int> arr, int target)
+{
+    sort(arr.begin(), arr.end());
+
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left < right)
+    {
+        int sum = arr[left] + arr[right];
+
+        if (sum == target)
+        {
+            cout << arr[left] << " " << arr[right];
+            return;
+        }
+        else if (sum < target)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    vector<int> vec = {9, 5, 7, 2, 1};
+    int target = 11;
+    two_sum(vec, target);
+    return 0;
+}
